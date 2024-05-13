9bb6561cc26491f082986fea84dbd0955aaec3e7
FarahRefaie <farahrefaie.fr@gmail.com>
2024-05-13 21:36:59 +0300

    front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,15 @@ import React from 'react'
 import "./App.css";
 const App = () => {
   return (
-   <div className="App">
-      <h1>Hello World!</h1>
+    <div className="App">
+      <Navigation />
+      <Routes>
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<landing />} />
+        </Routes>
     </div>
-  )
-}
+  
+  )}
 
 export default App
